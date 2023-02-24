6fc46116e77a38fe5d1e4f3ed8ad75babfe72fcf
Antoine Mariani <antoine.mariani@me.com>
2023-02-24 09:57:56 +0100

    Fixing resume link in Navbar

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -34,7 +34,7 @@ export default function Navbar() {
             className="hidden lg:flex lg:gap-4 lg:text-xs lg:font-bold lg:uppercase lg:tracking-wide lg:text-gray-500"
           >
             <a
-              src={resume}
+              href={resume}
               className="block h-16 border-b-4 border-transparent leading-[4rem] hover:border-current hover:text-red-700"
             >
               Resume
